e8de2e69ec2a9673e5b6585f27566541f38715b5
oscarArriaza <arriaza22598@vk.com>
2025-05-15 12:31:20 +0200

    commit

--- resources/js/app.js
+++
@@ -1,4 +0,0 @@
-import "./bootstrap";
-
-import * as bootstrap from "bootstrap";
-
--- resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-
-@section('title')
-    Home
-@endsection
-
-@section('content')
-    {{ $user->nombre_u }}
-@endsection
